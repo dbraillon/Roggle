3bb246243fa010dceabc5b32959b083260ef19a0
dbraillon <d.braillon@outlook.com>
2015-10-14 14:00:29 +0200

    Add tests for EmailRoggle, hide app.config credentials and add ssl support in EmailRoggle.

--- src/Roggle.Tests/RoggleTests.cs
+++ src/Roggle.Tests/RoggleTests.cs
@@ -18,9 +18,19 @@ namespace Roggle.Tests
         }
 
         [TestMethod]
-        public void EventLogTests()
+        public void EventLogRoggleTests()
         {
             GRoggle.Use<EventLogRoggle>();
+            GRoggle.Current.WriteDebug("I'm testing a debug message !");
+            GRoggle.Current.WriteInformation("I'm testing an information message !");
+            GRoggle.Current.WriteWarning("I'm testing a warning message !");
+            GRoggle.Current.WriteError("I'm testing an error message !");
+        }
+
+        [TestMethod]
+        public void EmailRoggleTests()
+        {
+            GRoggle.Use<EmailRoggle>();
             GRoggle.Current.WriteDebug("I'm testing a debug message !");
             GRoggle.Current.WriteInformation("I'm testing an information message !");
             GRoggle.Current.WriteWarning("I'm testing a warning message !");
