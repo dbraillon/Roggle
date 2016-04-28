6b8bdb26e24c1288ba3cda5ccc2b229f6fad8cc7
Davy Braillon <davy.braillon@live.com>
2016-04-28 11:14:17 +0200

    Change name of Roggle.Web to Roggle.Overseer.

--- src/Roggle.Overseer/Properties/AssemblyInfo.cs
+++ src/Roggle.Overseer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Roggle.Web")]
+[assembly: AssemblyTitle("Roggle.Overseer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Roggle.Web")]
+[assembly: AssemblyProduct("Roggle.Overseer")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
